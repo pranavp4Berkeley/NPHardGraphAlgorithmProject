5d509e8e8668bd75c9b57d778514e226ff5232fe
Rithwik Ediga Lakhamsani <ricky.ediga@gmail.com>
2019-12-04 11:02:05 -0800

    stuff

--- solver.py
+++ solver.py
@@ -51,8 +51,10 @@ def metric_TSP_solver(G, starting_car_location, list_of_homes):
     T = nx.minimum_spanning_tree(G)
     # Generates a DFS call sequence.
     marked = {}
+    
     for node in G.nodes:
         marked[node] = False
+
     dfs_traversal = []
     def gen_dfs(node):
         dfs_traversal.append(node)
@@ -69,7 +71,7 @@ def metric_TSP_solver(G, starting_car_location, list_of_homes):
     # Saves indices of visited locations.
     visited = {}
     # List of locations represeting the car path.
-    tour = dfs_traversal[:]
+    tour = []
     # Maps locations to the homes of the TAs who were dropped off at the location.
     dropoff_map = {}
     for i in range(len(dfs_traversal)):
@@ -84,16 +86,17 @@ def metric_TSP_solver(G, starting_car_location, list_of_homes):
             dropoff_cost, dropped_homes = compute_dropoff_cost(G, location_loop, list_of_homes)
             # If it's better to drop all the TAs living withing the loop at the start than to drive the entire loop.
             if (dropoff_cost < drive_cost):
-                for loc in location_loop:
-                    tour.pop(loc)
+                # for loc in location_loop[1:]:
+                #     tour.remove(loc)
                 # Storing for output file.
                 dropoff_map[node] = dropped_homes
                 # Removing homes of the TAs that were dropped off and creating a SUPER home.
-                for home in dropped_homes:
-                    list_of_homes.remove(home)
-                list_of_homes.append(node)
+                # for home in dropped_homes:
+                #     list_of_homes.remove(home)
+                # list_of_homes.append(node)
             else: # Else, it's better to drive the loop. Update the index of visited location.
                 visited[node] = i
+                dropoff_map[node] = [] # Don't drop off any TA, but still visit this location.
     return tour, dropoff_map
 
 
@@ -103,7 +106,7 @@ def compute_drive_cost(G, location_loop):
     for i in range(len(location_loop) - 1):
         source = location_loop[i]
         dest = location_loop[i + 1]
-        cost += (2 / 3) * (G[source][dest]['weight'])
+        cost += (2.0 / 3.0) * (G[source][dest]['weight'])
     return cost
 
 # Calculates the cost of dropping off all TAs who live within the loop at the start.
@@ -200,3 +203,88 @@ if __name__=="__main__":
     else:
         input_file = args.input
         solve_from_file(input_file, output_directory, params=args.params)
+
+// Include SSE intrinsics
+#if defined(_MSC_VER)
+#include <intrin.h>
+#elif defined(__GNUC__) && (defined(__x86_64__) || defined(__i386__))
+#include <immintrin.h>
+#include <x86intrin.h>
+#endif
+
+// Include OpenMP
+#include <omp.h>
+
+#include "mandelbrot.h"
+#include "parameters.h"
+
+void print_part(__m256d item, char * name){
+    double * item_double = (double*) &item;
+    printf("%s: ", name);
+    for(int j = 0; j < 4; j++){
+        printf("%d ", item_double[j]);
+    }
+    printf("\n");
+}
+
+uint32_t iterations(__m256d point_r_256, __m256d point_i_256, __m256d thresholds, struct parameters params) {
+    __m256d z_r_s_256 = _mm256_set1_pd(0);
+    __m256d z_i_s_256 = _mm256_set1_pd(0);
+    int num_zeros = 0;
+    for (int i = 1; i <= params.maxiters; i++) {
+
+        __m256d new_z_r_s_256 = _mm256_sub_pd(_mm256_mul_pd(z_r_s_256, z_r_s_256), _mm256_mul_pd(z_i_s_256, z_i_s_256));
+        __m256d new_z_i_s_256 = _mm256_mul_pd(_mm256_mul_pd(z_r_s_256, z_i_s_256), _mm256_set1_pd(2));
+        z_r_s_256 = _mm256_add_pd(new_z_r_s_256, point_r_256);
+        z_i_s_256 = _mm256_add_pd(new_z_i_s_256, point_i_256);
+
+        __m256d magnitudes = _mm256_add_pd(_mm256_mul_pd(z_r_s_256,z_r_s_256), _mm256_mul_pd(z_i_s_256, z_i_s_256));
+
+        __m256d comparers = _mm256_cmp_pd(magnitudes, thresholds, 29);
+        double comparer_double[4];
+        _mm256_storeu_pd(comparer_double, comparers);
+        int temp_num_zeros = 0;
+        for(int j = 0; j < 4; j++){
+            if(comparer_double[j] == 0){
+                temp_num_zeros++;
+            }
+        }
+        num_zeros = temp_num_zeros;
+        if(num_zeros == 4){
+            return 4;
+        }
+    }
+    return num_zeros;
+}
+
+void mandelbrot(struct parameters params, double scale, int32_t *num_pixels_in_set) {
+
+    int32_t num_zero_pixels = 0;
+    int i, j;
+
+    for (i = params.resolution; i >= -params.resolution; i--) {
+        // #pragma omp parallel for reduction(+:num_zero_pixels) private(j, flag, point_r, point_i, z_r, z_i, new_z_r, new_z_i)
+        for (j = -params.resolution; j <= params.resolution; j += 4) {
+            
+            __m256d j_256 = _mm256_set_pd((double) (j+3),(double) (j+2),(double) (j+1),(double) (j));
+            __m256d i_256 = _mm256_set1_pd(i);
+            __m256d center_real_256 = _mm256_set1_pd(creal(params.center));
+            __m256d center_imag_256 = _mm256_set1_pd(cimag(params.center));
+            __m256d scale_256 = _mm256_set1_pd(scale);
+            __m256d resolution_256 = _mm256_set1_pd(params.resolution);
+
+            __m256d point_r_256 = _mm256_add_pd(center_real_256, _mm256_mul_pd(j_256, _mm256_div_pd(scale_256, resolution_256)));
+            __m256d point_i_256 = _mm256_add_pd(center_imag_256, _mm256_mul_pd(i_256, _mm256_div_pd(scale_256, resolution_256)));
+            __m256d thresholds = _mm256_set1_pd(params.threshold * params.threshold);
+
+            double d[4];
+            _mm256_storeu_pd(d,point_r_256);
+            printf("%f %f %f %f \n", d[0], d[1], d[2], d[3]);
+
+            int flag = iterations(point_r_256, point_i_256, thresholds, params);
+
+            num_zero_pixels+=flag;
+        }
+    }
+    *num_pixels_in_set = num_zero_pixels;
+}

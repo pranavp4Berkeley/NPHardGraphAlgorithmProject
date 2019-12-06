284f50e7bd99e0986e7cdf6e33ef75c958ae10eb
Abhi Arya <abhiarya@berkeley.edu>
2019-12-05 16:30:21 -0800

    started new algo

--- solver.py
+++ solver.py
@@ -10,6 +10,7 @@ import string
 import math
 import matplotlib.pyplot as plt
 import networkx as nx
+import copy
 
 from student_utils import *
 """
@@ -49,6 +50,61 @@ def solve(list_of_locations, list_of_homes, starting_car_location, adjacency_mat
         indexed_dropoff_map[location_to_index[loc]] = [location_to_index[home] for home in dropoff_map[loc]]
     return tour, indexed_dropoff_map
 
+def simplified_metric_TSP_solver(G, starting_car_location, list_of_homes):
+    T = nx.minimum_spanning_tree(G)
+
+def find_dropoff_locations(T, starting_car_location, list_of_homes, G):
+    stack = []
+    stack.append(starting_location)
+    visited = {}
+    dropoff_locations = []
+    dropped_homes = []
+    while len(stack) > 0:
+        node = stack.pop()
+        visited[node] = True
+        dropoff_cost, subtree_homes = root_dropoff_cost(T, node, list_of_homes, G, copy.deepcopy(visited))
+        drive_cost = root_drive_cost(T, node, list_of_homes, G, copy.deepcopy(visited))
+        if 0 < dropoff_cost and dropoff_cost < drive_cost:
+            dropoff_locations.append(node)
+            dropped_homes += subtree_homes
+        for neighbor in T.neighbors(node):
+            if not visited[neighbor]:
+                stack.append(neighbor)
+    return dropoff_locations, dropped_homes
+
+
+# Returns a list of all TA homes contained in the subtree rooted at starting_location.
+def find_subtree_homes(T, starting_location, list_of_homes, visited):
+    subtree_homes = []
+    stack = []
+    stack.append(starting_location)
+    while len(stack) > 0:
+        node = stack.pop()
+        visited[node] = True
+        if node in list_of_homes:
+            subtree_homes.append(node)
+        for neighbor in T.neighbors(node):
+            if not visited[neighbor]:
+                stack.append(neighbor)
+    return subtree_homes
+
+# Calculates cost of dropping off all the TA's whose homes are in the subtree rooted at starting_location.
+def root_dropoff_cost(T, starting_location, list_of_homes, G, visited):
+    subtree_homes = find_subtree_homes(T, starting_location, list_of_homes, visited)
+    dropoff_cost = 0
+    for home in subtree_homes:
+        walk_distance, _ = nx.single_source_dijkstra(G, source=starting_location, target=node, cutoff=None, weight='weight')
+        dropoff_cost += walk_distance
+    return dropoff_cost, subtree_homes
+
+# Calculates the cost of a tour that visits all TA homes contained in the subtree rooted at starting_location.
+# NOTE: not guaranteed to be min cost tour, but pretty decent heuristic.
+def root_drive_cost(T, starting_location, list_of_homes, G, visited):
+    subtree_homes = find_subtree_homes(T, starting_location, list_of_homes, visited)
+    drive_tour = find_tour(G, starting_car_location, subtree_homes)
+    driving_cost = compute_drive_cost(G, drive_tour)
+    return driving_cost
+
 def metric_TSP_solver(G, starting_car_location, list_of_homes):
     T = nx.minimum_spanning_tree(G)
 
@@ -68,7 +124,7 @@ def metric_TSP_solver(G, starting_car_location, list_of_homes):
 
     gen_dfs(starting_car_location)
 
-    print(dfs_traversal)
+    #print(dfs_traversal)
 
     # Saves indices of visited locations.
     visited = {}
@@ -96,8 +152,10 @@ def metric_TSP_solver(G, starting_car_location, list_of_homes):
 
                 # Removing homes of the TAs that were dropped off.
                 for home in dropped_homes:
-                    if home in list_of_homes: # Not sure if this is necessary.
+                    if home in list_of_homes: # Not sure why this is necessary.
                         list_of_homes.remove(home)
+                    # if home in locations:
+                    #     locations.remove(home)
                     # Also removing super dropoff locations within the location loop.
                     if home in dropoff_map:
                         del dropoff_map[home]
@@ -111,9 +169,9 @@ def metric_TSP_solver(G, starting_car_location, list_of_homes):
                 for loc in location_loop:
                     if loc in list_of_homes:
                         locations.append(loc) # The final tour should include all homes in the location loop.
-    print(len(locations))
-    print(locations)
-    print(dropoff_map)
+    #print(len(locations))
+    #print(locations)
+    #print(dropoff_map)
     tour = find_tour(G, starting_car_location, locations)
     return tour, dropoff_map
 
